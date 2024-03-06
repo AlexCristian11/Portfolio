f15b7c92c23361a5939bf9a8e10c0ba882476e80
AlexCristian11 <alexpopescu2002@yahoo.com>
2024-03-07 01:25:24 +0200

    Created the Main component of the app

---
+++ src/components/Main.tsx
@@ -0,0 +1,75 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+import Info from './Info';
+
+function Main() {
+
+    const [activeContent, setActiveContent] = useState<string | null>(null);
+
+    const openContent = (content: string) => setActiveContent(content);
+    const closeContent = () => setActiveContent(null);
+
+  return (
+    <>
+        <MainComponent>
+            <h1>Alexandru-Cristian Popescu</h1>
+            <p>Software Engineer</p>
+            <ul id="list">
+                <li onClick={() => openContent('info')}>Info</li>
+                <li onClick={() => openContent('about')}>About</li>
+                <li onClick={() => openContent('projects')}>Projects</li>
+                <li onClick={() => openContent('contact')}>Contact</li>
+            </ul>
+            {activeContent === 'info' && (
+                <InfoContainer>
+                    <Info isOpen={true} onClose={closeContent}/>
+                </InfoContainer>
+            )}
+        </MainComponent>
+    </>
+  )
+}
+
+const MainComponent = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    h1 {
+        font-size: 2.75rem;
+        margin-top: 1rem;
+    }
+
+    p {
+        font-size: 1.25rem;
+        font-weight: 500;
+        font-style: italic;
+        margin-top: 1rem;
+    }
+
+    ul {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin-top: 1rem;
+    }
+
+    li {
+        font-size: 2rem;
+        margin-top: .75rem;
+    }
+
+    #list {
+        list-style-type: none;
+    }
+`;
+
+const InfoContainer = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+`;
+
+export default Main;
