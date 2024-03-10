8187bc47810928d6df89ce95ddcf2aee1dd08bb4
AlexCristian11 <alexpopescu2002@yahoo.com>
2024-03-10 17:22:48 +0200

    Created animation functionality

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,24 +1,61 @@
-import styled from 'styled-components';
+import { useEffect } from 'react';
+import styled, { keyframes, css } from 'styled-components';
 
 interface InfoProps {
     isOpen: boolean;
     onClose: () => void;
 }
 
+const slideDown = keyframes`
+ from {
+    transform: translateY(-100%);
+ }
+ to {
+    transform: translateY(0);
+ }
+`;
+
+const slideUp = keyframes`
+ from {
+    transform: translateY(0);
+ }
+ to {
+    transform: translateY(-100%);
+ }
+`;
+
+
 const Info: React.FC<InfoProps> = ({ isOpen, onClose }) => {
+
+    useEffect(() => {
+        console.log('isOpen:', isOpen);
+       }, [isOpen]);
+
   return (
     <>
         <InfoContainer isOpen={isOpen}>
+            <div>
+                <h1>Info</h1>
+            </div>
             <div className="close-button" onClick={onClose}></div>
         </InfoContainer>
     </>
   )
 }
 
-const InfoContainer = styled.div`
+const InfoContainer = styled.div<{ isOpen: boolean }>`
+    position: fixed;
+    top: 0;
+    left: 0;
     width: 100%;
     height: 100%;
     z-index: 1000;
+    background-color: rgba(0, 0, 0, 0.8);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    animation: ${props => (props.isOpen ? css`${slideDown} 0.7s forwards` : css`${slideUp} 0.7s forwards`)};
+    animation-fill-mode: forwards;
 
     .close-button {
         width: 50px;
